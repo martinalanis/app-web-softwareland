745c53a02499ffef15eec332d2d93148ab184274
martinalanis <martinalanis.dev@gmail.com>
2021-05-27 15:53:14 -0500

    feat: Components added in index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Layout from "../components/Layout"
 import Header from "../components/Header"
 import Hero from "../components/Hero"
+import Feature from "../components/Feature"
+import Service from "../components/Service"
+import About from "../components/About"
+import Footer from "../components/Footer"
 const Index = () => {
   return (
     <Layout pageTitle="Web App Softwareland">
       <Header />
       <Hero />
+      <Feature />
+      <Service />
+      <About />
+      <Footer />
     </Layout>
   )
 }
