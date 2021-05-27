50a6be3579f47e75227beb73fcba12b456df5f3f
martinalanis <martinalanis.dev@gmail.com>
2021-05-27 15:45:26 -0500

    feat: Feature component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Layout from "../components/Layout"
 import Header from "../components/Header"
+import Hero from "../components/Hero"
 const Index = () => {
   return (
     <Layout pageTitle="Web App Softwareland">
       <Header />
+      <Hero />
     </Layout>
   )
 }
